1134af4098f6c869014d6a0d3940cfc253c21813
Voramate Kittichotirut <voramate05@outlook.co.th>
2026-02-21 14:15:45 +0700

    fix: remove Rickroll🕺🏼

--- app/application/file/page.tsx
+++ app/application/file/page.tsx
@@ -2,7 +2,7 @@
 
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {
-    faArrowLeft,
+    faArrowLeft, faDownload,
     faFaceLaughBeam, faFileLines, faFloppyDisk,
     faFolderOpen,
     faGraduationCap,
@@ -27,7 +27,7 @@ export default function fileUploadPage() {
         { type: "file_national_id", accept: ".png,.jpg,.jpeg,.pdf", label: "สำเนาบัตรประชาชน", description: (<>สามารถใช้สำเนาบัตรนักเรียนแทนได้ <b>(เซ็นสำเนาถูกต้อง)</b></>), fileSize: 3, icon: faIdCard},
         { type: "file_pp_1", accept: ".png,.jpg,.jpeg,.pdf", label: "สำเนาระเบียนแสดงผลการเรียน (ปพ.1)", description: (<>สามารถใช้เอกสารแสดงผลการเรียนภาคการศึกษาล่าสุดแทนได้ <b>(เซ็นสำเนาถูกต้อง - ต้องมีผลการเรียน GPAX)</b></>), fileSize: 3, icon: faFileLines},
         { type: "file_pp_7", accept: ".png,.jpg,.jpeg,.pdf", label: "สำเนาใบรับรองสถานภาพการเป็นนักเรียน (ปพ.7)", description: (<>สามารถใช้เอกสารรับรองการเป็นนักเรียนแทนได้ เช่น สำเนาบัตรนักเรียน <b>(เซ็นสำเนาถูกต้อง)</b></>), fileSize: 3, icon: faGraduationCap},
-        { type: "file_parent_permission", accept: ".png,.jpg,.jpeg,.pdf", label: "เอกสารขออนุญาตผู้ปกครอง", description: (<a className="underline" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank"><b>กดที่นี่เพื่อดาวน์โหลดไฟล์เอกสารขออนุญาตผู้ปกครอง</b></a>), fileSize: 3, icon: faPersonBreastfeeding}
+        { type: "file_parent_permission", accept: ".png,.jpg,.jpeg,.pdf", label: "เอกสารขออนุญาตผู้ปกครอง", description: (<a className="underline" href="https://storage.comcamp.io/web-assets/เอกสารขออนุญาตผู้ปกครอง.pdf" download="เอกสารขออนุญาตผู้ปกครอง.pdf" target="_blank"><FontAwesomeIcon icon={faDownload} /><b>กดที่นี่เพื่อดาวน์โหลดไฟล์เอกสารขออนุญาตผู้ปกครอง</b></a>), fileSize: 3, icon: faPersonBreastfeeding}
     ]
 
     return (
